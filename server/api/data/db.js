8d95cb420db71917eb1ebb5309c4408a15f7164d
Erimilson Silva <eesr@discente.ifpe.edu.br>
2024-04-01 19:58:41 -0300

    ajuste nas urls

--- server/api/data/db.js
+++ server/api/data/db.js
@@ -3,7 +3,6 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 });

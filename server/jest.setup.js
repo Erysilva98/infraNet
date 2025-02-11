2a148bd7b8fec35100d5bb7b2bf6035403f8aae7
Erimilson Silva <eesr@discente.ifpe.edu.br>
2025-02-10 21:05:43 -0300

    coppy do db para test

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,11 +1,7 @@
 const sequelize = require("./api/data/db");
 require('dotenv').config({ path: '.env' });
 
-
 beforeAll(async () => {
-    await sequelize.sync(); 
-});
+    await sequelize.authenticate();
 
-afterAll(async () => {
-    await sequelize.close(); // Fecha a conexão do banco após os testes
 });
